8c9110f74450325728165df2a1b3e3401ef7a216
Feyannibelli <fyann06@gmail.com>
2025-05-29 10:56:42 -0300

    password change

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const { Sequelize } = require('sequelize'); // Corregido: 'require' en lugar de 
 const sequelize = new Sequelize(
     process.env.DB_NAME || 'iotdb',
     process.env.DB_USER || 'iotuser',
-    process.env.DB_PASSWORD || 'tu_password_segura',
+    process.env.DB_PASSWORD || 'tu_password_segura_123',
     {
         host: process.env.DB_HOST || 'tu-instancia-sql-privada.aws', // recordar dar la direccion ip de la instancia o el DNS
         port: process.env.DB_PORT || 5432,
